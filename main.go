b6798bf6930f1845067b641696937d3e566a7138
Matt Joiner <anacrolix@gmail.com>
2017-01-29 18:19:14 +1100

    Add cache capacity and dht public IP flags

--- main.go
+++ main.go
@@ -5,21 +5,47 @@ import (
 	"net"
 	"net/http"
 
+	"github.com/anacrolix/dht"
 	_ "github.com/anacrolix/envpprof"
+	"github.com/anacrolix/missinggo/filecache"
 	"github.com/anacrolix/tagflag"
+	"github.com/anacrolix/torrent"
+	"github.com/anacrolix/torrent/iplist"
+	"github.com/anacrolix/torrent/storage"
 
 	"github.com/anacrolix/confluence/confluence"
 )
 
+var flags = struct {
+	Addr          string
+	DHTPublicIP   net.IP
+	CacheCapacity tagflag.Bytes
+}{
+	Addr:          "localhost:8080",
+	CacheCapacity: 10 << 30,
+}
+
+func newTorrentClient() (ret *torrent.Client, err error) {
+	blocklist, err := iplist.MMapPacked("packed-blocklist")
+	if err != nil {
+		log.Print(err)
+	}
+	fc := filecache.NewCache("filecache")
+	fc.SetCapacity(flags.CacheCapacity.Int64())
+	storageProvider := fc.AsResourceProvider()
+	return torrent.NewClient(&torrent.Config{
+		IPBlocklist:    blocklist,
+		DefaultStorage: storage.NewResourcePieces(storageProvider),
+		DHTConfig: dht.ServerConfig{
+			PublicIP: flags.DHTPublicIP,
+		},
+	})
+}
+
 func main() {
 	log.SetFlags(log.Flags() | log.Lshortfile)
-	flags := struct {
-		Addr string
-	}{
-		Addr: "localhost:8080",
-	}
 	tagflag.Parse(&flags)
-	cl, err := confluence.NewDefaultTorrentClient()
+	cl, err := newTorrentClient()
 	if err != nil {
 		log.Fatalf("error creating torrent client: %s", err)
 	}
