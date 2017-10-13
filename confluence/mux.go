948e4fdb83e981ba933b7b5da0e9d3caed65c03c
Matt Joiner <anacrolix@gmail.com>
2017-10-13 14:14:47 +1100

    gzip /fileState output, it gets very large (600KB+)

--- confluence/mux.go
+++ confluence/mux.go
@@ -3,6 +3,8 @@ package confluence
 import (
 	"net/http"
 
+	"github.com/anacrolix/missinggo/httptoo"
+
 	"github.com/justinas/alice"
 )
 
@@ -13,6 +15,9 @@ func init() {
 	mux.HandleFunc("/status", statusHandler)
 	mux.Handle("/info", alice.New(withTorrentContext).ThenFunc(infoHandler))
 	mux.Handle("/events", alice.New(withTorrentContext).ThenFunc(eventHandler))
-	mux.Handle("/fileState", alice.New(withTorrentContext).ThenFunc(fileStateHandler))
+	mux.Handle("/fileState", alice.New(
+		withTorrentContext,
+		httptoo.GzipHandler,
+	).ThenFunc(fileStateHandler))
 	mux.Handle("/metainfo", alice.New(withTorrentContext).ThenFunc(metainfoHandler))
 }
