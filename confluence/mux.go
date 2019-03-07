7e5200201fe57de63f12465fb5d5fb0c6e846d05
Waleed Gadelkareem <gadelkareem@gmail.com>
2019-03-07 01:11:48 +0100

    remove duplicated routes

--- confluence/mux.go
+++ confluence/mux.go
@@ -13,11 +13,10 @@ func init() {
 	mux.Handle("/data", alice.New(withTorrentContext).ThenFunc(dataHandler))
 	mux.HandleFunc("/status", statusHandler)
 	mux.Handle("/info", alice.New(withTorrentContext).ThenFunc(infoHandler))
-	mux.Handle("/metainfo", alice.New(withTorrentContext).ThenFunc(metainfoGetHandler))
 	mux.Handle("/events", alice.New(withTorrentContext).ThenFunc(eventHandler))
 	mux.Handle("/fileState", alice.New(
 		withTorrentContext,
 		httptoo.GzipHandler,
 	).ThenFunc(fileStateHandler))
-	mux.Handle("/metainfo", alice.New(withTorrentContext).ThenFunc(metainfoPostHandler))
+	mux.Handle("/metainfo", alice.New(withTorrentContext).ThenFunc(metainfoHandler))
 }
